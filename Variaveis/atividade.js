3d3b027a72a305ef491f74a23161500fda7b2eb3
Rebeca Reis <118316578+RebecaSilva25@users.noreply.github.com>
2023-07-12 15:05:03 -0300

    Add files via upload

---
+++ Variaveis/atividade.js
@@ -0,0 +1,17 @@
+var mensagem = "Resultado";
+        alert(mensagem);
+        
+        var x = 2;
+        var y = 3;
+
+        var z = x + y;
+        document.getElementById("soma").innerHTML = z;
+
+        var z = x / y;
+        document.getElementById("divisao").innerHTML = y;
+
+        var z = x * y;
+        document.getElementById("multi").innerHTML = z;
+
+        var z = x < y;
+        document.getElementById("comparacao").innerHTML = z;
